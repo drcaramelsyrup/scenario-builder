93996f756427498fd8143d7af6922092f4bad5ce
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2017-11-06 08:15:25 -0800

    Data and satisfaction

--- src/el-logic.js
+++ src/el-logic.js
@@ -2,6 +2,10 @@
 import ELTree from './el-tree';
 import ELNode from './el-node';
 
+const satisfies = (tree: ELTree, statement: string) => {
+	return typeof retrieve(tree, statement) !== 'undefined';
+};
+
 const claim = (tree: ELTree, statement: string) => {
 	const treeRoot = tree.root;
 	if (treeRoot.isRoot !== true)
@@ -9,7 +13,7 @@ const claim = (tree: ELTree, statement: string) => {
 	if (treeRoot.value == null)
 		throw new Error('Root has no value');
 
-	const nodeValues = statement.split(/[.!]+/);
+	const nodeValues = parseNodeValues(statement);
 	if (nodeValues.length === 0 || treeRoot.value !== nodeValues.shift())
 		return;
 
@@ -34,19 +38,23 @@ const claim = (tree: ELTree, statement: string) => {
 
 };
 
-const children = (tree: ELTree, nodeValue: string) => {
+const children = (tree: ELTree, statement: string) => {
 	const treeRoot = tree.root;
-	return retrieve(tree, nodeValue).children.map((child) => child.value);
-}
+	const retrieved = retrieve(tree, statement);
+	if (typeof retrieved === 'undefined')
+		throw new Error('Could not find node for \''+statement+'\' in tree '+treeRoot.value+'!');
+
+	return retrieved.children.map((child) => child.value);
+};
 
 const retrieve = (tree: ELTree, nodeValue: string) => {
-	const nodeValues = nodeValue.split(/[.!]+/);
+	const nodeValues = parseNodeValues(nodeValue);
 	const rootValue = nodeValues.shift();
 	if (rootValue !== tree.root.value)
 		throw new Error('Could not find root node in tree!');
 
 	return retrieveNode(tree.root, nodeValues);
-}
+};
 
 const retrieveNode = (node: ELNode, childValues: Array<string> = []) => {
 	if (childValues.length === 0)
@@ -56,13 +64,26 @@ const retrieveNode = (node: ELNode, childValues: Array<string> = []) => {
 	const child = childWithValue(node, childValue);
 
 	if (typeof child === 'undefined')
-		throw new Error('Could not find node in tree!');
+		return child;	// invalid node
 
 	return retrieveNode(child, childValues);
-}
+};
 
 const childWithValue = (node: ELNode, childValue: string = '') => {
-	return node.children.find((child) => { return child.value === childValue; });
-}
+	if (node.operator === '' && node.children.length !== 0)
+		throw new Error('Invalid node \''+node.value+'\' with empty operator and non-zero children');
 
-export { claim, children };
+	if (!(node.operator === '.' || node.operator === '!' || node.operator === ''))
+		throw new Error('Invalid node \''+node.value+'\' with unrecognized operator \''+node.operator+'\'');
+
+	if (node.children.length === 0 && node.operator === '.' || node.operator === '!')
+		throw new Error('Invalid node \''+node.value+'\' with operator \''+node.operator+'\' and no children');
+
+	return node.children.find((child) => { return child.value === childValue; });
+};
+
+const parseNodeValues = (statement: string) => {
+	return statement.split(/[.!]+/);
+};
+
+export { claim, children, satisfies };
